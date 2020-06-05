d0a10f2cb5ed22bad6b84375a1ab0e8213e008f2
gegy1000 <gegy1000@gmail.com>
2020-06-05 20:02:58 +0200

    remove from whitelist and blacklist with command

--- src/main/java/com/lovetropics/survivalplus/SPConfigs.java
+++ src/main/java/com/lovetropics/survivalplus/SPConfigs.java
@@ -2,6 +2,8 @@ package com.lovetropics.survivalplus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -16,41 +18,69 @@ import net.minecraftforge.fml.config.ModConfig;
 
 @EventBusSubscriber(modid = SurvivalPlus.MODID, bus = Bus.MOD)
 public class SPConfigs {
-
+	
 	public static class Server {
-
+		
 		final ConfigValue<List<? extends String>> whitelist;
 		final ConfigValue<List<? extends String>> blacklist;
-
+		
 		ItemFilter filter;
-
+		
 		Server(ForgeConfigSpec.Builder builder) {
 			whitelist = builder.defineList("whitelist", new ArrayList<>(), Predicates.alwaysTrue());
 			blacklist = builder.defineList("blacklist", new ArrayList<>(), Predicates.alwaysTrue());
 		}
 		
 		public void addWhitelist(String entry) {
-			@SuppressWarnings("unchecked")
-			List<String> whitelist = (List<String>) this.whitelist.get();
-			whitelist.add(entry);
-			
-			this.whitelist.set(whitelist);
-			this.whitelist.save();
-			
-			this.resetFilter();
+			this.modifyBlacklist(whitelist -> whitelist.add(entry));
 		}
 		
 		public void addBlacklist(String entry) {
+			this.modifyBlacklist(blacklist -> blacklist.add(entry));
+		}
+		
+		public boolean removeWhitelist(String entry) {
+			return this.modifyWhitelist(whitelist -> whitelist.remove(entry));
+		}
+		
+		public boolean removeBlacklist(String entry) {
+			return this.modifyBlacklist(blacklist -> blacklist.remove(entry));
+		}
+		
+		@SuppressWarnings("unchecked")
+		public Stream<String> whitelist() {
+			return ((List<String>) this.whitelist.get()).stream();
+		}
+		
+		@SuppressWarnings("unchecked")
+		public Stream<String> blacklist() {
+			return ((List<String>) this.blacklist.get()).stream();
+		}
+		
+		public <T> T modifyWhitelist(Function<List<String>, T> function) {
+			@SuppressWarnings("unchecked")
+			List<String> whitelist = (List<String>) this.whitelist.get();
+			T result = function.apply(whitelist);
+			
+			this.whitelist.set(whitelist);
+			this.whitelist.save();
+			this.resetFilter();
+			
+			return result;
+		}
+		
+		public <T> T modifyBlacklist(Function<List<String>, T> function) {
 			@SuppressWarnings("unchecked")
 			List<String> blacklist = (List<String>) this.blacklist.get();
-			blacklist.add(entry);
+			T result = function.apply(blacklist);
 			
 			this.blacklist.set(blacklist);
 			this.blacklist.save();
-			
 			this.resetFilter();
+			
+			return result;
 		}
-
+		
 		public ItemFilter getFilter() {
 			if (filter == null) {
 				filter = ItemFilter.fromStrings(whitelist.get(), blacklist.get());
@@ -62,20 +92,21 @@ public class SPConfigs {
 			filter = null;
 		}
 	}
-
+	
 	static final ForgeConfigSpec serverSpec;
 	public static final Server SERVER;
+	
 	static {
 		final Pair<Server, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Server::new);
 		serverSpec = specPair.getRight();
 		SERVER = specPair.getLeft();
 	}
-
+	
 	@SubscribeEvent
 	public static void onLoad(final ModConfig.Loading configEvent) {
 		SERVER.resetFilter();
 	}
-
+	
 	@SubscribeEvent
 	public static void onReload(final ModConfig.Reloading configEvent) {
 		SERVER.resetFilter();
