cb0c41deb0b661e35275e6383e4c9f8cd5d73314
tterrag <tterrag1098@gmail.com>
2020-06-05 22:38:03 -0400

    Delete marked item stacks when tossed

--- src/main/java/com/lovetropics/survivalplus/container/PlayerInventoryHooks.java
+++ src/main/java/com/lovetropics/survivalplus/container/PlayerInventoryHooks.java
@@ -8,6 +8,7 @@ import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -28,6 +29,13 @@ public final class PlayerInventoryHooks {
 			
 			SurvivalPlusContainer container = new SurvivalPlusContainer(0, player.inventory);
 			event.setGui(new SurvivalPlusScreen(container, player.inventory, SurvivalPlusContainer.title()));
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onToss(ItemTossEvent event) {
+		if (SPStackMarker.isMarked(event.getEntityItem().getItem())) {
+			event.setCanceled(true);
 		}
 	}
 }
