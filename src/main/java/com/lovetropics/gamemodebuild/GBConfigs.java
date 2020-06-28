4e4bf99d98a176ed135b42165ab2bdf9afed84e6
David Panić <david@davidpanic.dev>
2020-06-28 02:04:17 +0200

    Made lists generic

--- src/main/java/com/lovetropics/gamemodebuild/GBConfigs.java
+++ src/main/java/com/lovetropics/gamemodebuild/GBConfigs.java
@@ -1,14 +1,7 @@
 package com.lovetropics.gamemodebuild;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Stream;
-
-import org.apache.commons.lang3.tuple.Pair;
-
-import com.google.common.base.Predicates;
-
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
@@ -16,105 +9,94 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
 
-@EventBusSubscriber(modid = GamemodeBuild.MODID, bus = Bus.MOD)
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@EventBusSubscriber (modid = GamemodeBuild.MODID, bus = Bus.MOD)
 public class GBConfigs {
-	
 	public static class Server {
-		
-		final ConfigValue<List<? extends String>> whitelist;
-		final ConfigValue<List<? extends String>> blacklist;
+		final Gson gson;
+		final Type listsConfigType = new TypeToken<Map<String, List<String>>>() {}.getType();
+
+		Map<String, List<String>> lists;
+		final ConfigValue<String> listsConfig;
+
 		final BooleanValue enabled;
-		
+
 		ItemFilter filter;
-		
+
 		Server(ForgeConfigSpec.Builder builder) {
-			whitelist = builder.defineList("whitelist", new ArrayList<>(), Predicates.alwaysTrue());
-			blacklist = builder.defineList("blacklist", new ArrayList<>(), Predicates.alwaysTrue());
+			gson = new Gson();
+			listsConfig = builder.define("lists", "", o -> true);
+
 			enabled = builder.comment("Enable SurvivalPlus for all players").define("enabled", true);
 		}
-		
-		public void addWhitelist(String entry, boolean save) {
-			this.modifyWhitelist(whitelist -> whitelist.add(entry), save);
+
+		void loadLists() {
+			String configValue = listsConfig.get();
+			lists = gson.fromJson(configValue, listsConfigType);
+			if (lists == null) lists = new HashMap<>();
 		}
-		
-		public void addBlacklist(String entry, boolean save) {
-			this.modifyBlacklist(blacklist -> blacklist.add(entry), save);
+
+		private void saveLists() {
+			listsConfig.set(gson.toJson(lists));
+			listsConfig.save();
 		}
-		
-		public boolean removeWhitelist(String entry, boolean save) {
-			return this.modifyWhitelist(whitelist -> whitelist.remove(entry), save);
+
+		public <T> T modifyList(String name, Function<List<String>, T> function, boolean save) {
+			List<String> list = lists.get(name);
+			if (list == null) list = new ArrayList<>();
+
+			T result = function.apply(list);
+			lists.put(name, list);
+
+			if (save) saveLists();
+			this.resetFilter();
+
+			return result;
 		}
-		
-		public boolean removeBlacklist(String entry, boolean save) {
-			return this.modifyBlacklist(blacklist -> blacklist.remove(entry), save);
+
+		public void addToList(String name, String entry, boolean save) {
+			modifyList(name, list -> {
+				if (list.contains(entry)) return true;
+				return list.add(entry);
+			}, save);
 		}
-		
-		public int clearWhitelist(boolean save) {
-			return GBConfigs.SERVER.modifyWhitelist(whitelist -> {
-				int size = whitelist.size();
-				whitelist.clear();
+
+		public boolean removeFromList(String name, String entry, boolean save) {
+			return this.modifyList(name, list -> list.remove(entry), save);
+		}
+
+		public int clearList(String name, boolean save) {
+			return this.modifyList(name, list -> {
+				int size = list.size();
+				list.clear();
 				return size;
 			}, save);
 		}
-		
-		public int clearBlacklist(boolean save) {
-			return GBConfigs.SERVER.modifyBlacklist(blacklist -> {
-				int size = blacklist.size();
-				blacklist.clear();
-				return size;
-			}, save);
+
+		public Stream<String> getListStream(String name) {
+			List<String> list = lists.get(name);
+			if (list == null) list = new ArrayList<>();
+
+			return list.stream();
 		}
-		
-		@SuppressWarnings("unchecked")
-		public Stream<String> whitelist() {
-			return ((List<String>) this.whitelist.get()).stream();
-		}
-		
-		@SuppressWarnings("unchecked")
-		public Stream<String> blacklist() {
-			return ((List<String>) this.blacklist.get()).stream();
-		}
-		
-		public <T> T modifyWhitelist(Function<List<String>, T> function, boolean save) {
-			@SuppressWarnings("unchecked")
-			List<String> whitelist = (List<String>) this.whitelist.get();
-			T result = function.apply(whitelist);
-			
-			this.whitelist.set(whitelist);
-			if (save) {
-				this.whitelist.save();
-			}
-			this.resetFilter();
-			
-			return result;
-		}
-		
-		public <T> T modifyBlacklist(Function<List<String>, T> function, boolean save) {
-			@SuppressWarnings("unchecked")
-			List<String> blacklist = (List<String>) this.blacklist.get();
-			T result = function.apply(blacklist);
-			
-			this.blacklist.set(blacklist);
-			if (save) {
-				this.blacklist.save();
-			}
-			this.resetFilter();
-			
-			return result;
-		}
-		
+
 		public ItemFilter getFilter() {
 			if (filter == null) {
-				filter = ItemFilter.fromStrings(whitelist.get(), blacklist.get());
+				filter = ItemFilter.fromStrings(lists.get("whitelist"), lists.get("blacklist"));
 			}
 			return filter;
 		}
-		
+
 		void resetFilter() {
 			filter = null;
 		}
-		
+
 		public void enable(boolean state) {
 			this.enabled.set(state);
 			this.enabled.save();
@@ -124,23 +106,27 @@ public class GBConfigs {
 			return enabled.get();
 		}
 	}
-	
-	static final ForgeConfigSpec serverSpec;
-	public static final Server SERVER;
-	
+
+	static final        ForgeConfigSpec serverSpec;
+	public static final Server          SERVER;
+
 	static {
 		final Pair<Server, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Server::new);
 		serverSpec = specPair.getRight();
 		SERVER = specPair.getLeft();
+
+		SERVER.loadLists();
 	}
-	
+
 	@SubscribeEvent
 	public static void onLoad(final ModConfig.Loading configEvent) {
+		SERVER.loadLists();
 		SERVER.resetFilter();
 	}
-	
+
 	@SubscribeEvent
 	public static void onReload(final ModConfig.Reloading configEvent) {
+		SERVER.loadLists();
 		SERVER.resetFilter();
 	}
 }
--- src/main/java/com/lovetropics/gamemodebuild/command/GamemodeBuildCommand.java
+++ src/main/java/com/lovetropics/gamemodebuild/command/GamemodeBuildCommand.java
@@ -1,32 +1,34 @@
 package com.lovetropics.gamemodebuild.command;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import javax.annotation.Nullable;
-
 import com.lovetropics.gamemodebuild.GBConfigs;
 import com.lovetropics.gamemodebuild.GamemodeBuild;
+import com.lovetropics.gamemodebuild.command.ItemFilterArgument.Result;
 import com.lovetropics.gamemodebuild.message.ListUpdateMessage;
 import com.lovetropics.gamemodebuild.state.GBPlayerStore;
 import com.lovetropics.gamemodebuild.state.GBServerState;
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.ISuggestionProvider;
 import net.minecraft.command.arguments.GameProfileArgument;
-import net.minecraft.entity.ai.brain.task.UpdateActivityTask;
+import net.minecraft.command.arguments.GameProfileArgument.IProfileProvider;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.network.PacketDistributor;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
@@ -34,149 +36,128 @@ import static net.minecraft.command.Commands.literal;
 public final class GamemodeBuildCommand {
 	private static final SimpleCommandExceptionType FILTER_DID_NOT_EXIST = new SimpleCommandExceptionType(new StringTextComponent("That filter did not exist!"));
 	
-	public static void register(CommandDispatcher<CommandSource> dispatcher) {
-		// @formatter:off
-		dispatcher.register(literal("build")
-				.then(literal("disable")
-					.requires(src -> src.hasPermissionLevel(4))
-					.executes(ctx -> enable(ctx, null, false))
-					.then(argument("player", GameProfileArgument.gameProfile())
-						.executes(ctx -> enable(ctx, GameProfileArgument.getGameProfiles(ctx, "player"), false))))
-				.then(literal("enable")
-						.requires(src -> src.hasPermissionLevel(4))
-						.executes(ctx -> enable(ctx, null, true))
-						.then(argument("player", GameProfileArgument.gameProfile())
-							.executes(ctx -> enable(ctx, GameProfileArgument.getGameProfiles(ctx, "player"), true))))
-				.then(literal("whitelist")
-					.requires(src -> src.hasPermissionLevel(4))
-					.then(literal("add")
-						.then(argument("item", ItemFilterArgument.itemFilter())
-						.executes(GamemodeBuildCommand::addWhitelist)))
-					.then(literal("remove")
-						.then(argument("item", ItemFilterArgument.itemFilter())
-						.suggests(whitelistSuggestions())
-						.executes(GamemodeBuildCommand::removeWhitelist))
-					.then(literal("clear")
-						.executes(GamemodeBuildCommand::clearWhitelist)))
-				)
-				.then(literal("blacklist")
-					.requires(src -> src.hasPermissionLevel(4))
-					.then(literal("add")
-						.then(argument("item", ItemFilterArgument.itemFilter())
-						.executes(GamemodeBuildCommand::addBlacklist)))
-						.then(literal("remove")
-							.then(argument("item", ItemFilterArgument.itemFilter())
-							.suggests(blacklistSuggestions())
-							.executes(GamemodeBuildCommand::removeBlacklist))
-						.then(literal("clear")
-							.executes(GamemodeBuildCommand::clearBlacklist)))
-				)
-		);
-		// @formatter:on
+	private static RequiredArgumentBuilder<CommandSource, IProfileProvider> getPlayerArg() {
+		return argument("player", GameProfileArgument.gameProfile());
 	}
+
+	private static RequiredArgumentBuilder<CommandSource, Result> getItemArg() {
+		return argument("item", ItemFilterArgument.itemFilter());
+	}
+
+	// @formatter:off
+	private static LiteralArgumentBuilder<CommandSource> getEnableLiteral(boolean enable) {
+		return literal(enable ? "enable" : "disable")
+			.requires(src -> src.hasPermissionLevel(4))
+			.executes(ctx -> enable(ctx, null, enable))
+			.then(
+				getPlayerArg()
+				.executes(ctx -> enable(ctx, GameProfileArgument.getGameProfiles(ctx, "player"), enable))
+			);
+	}
+
+	private static LiteralArgumentBuilder<CommandSource> getListLiteral(String name) {
+		return literal(name)
+			.requires(src -> src.hasPermissionLevel(4))
+			.then(
+				literal("add")
+				.then(
+					getItemArg()
+					.executes(getListAddCommand(name))
+				)
+			)
+			.then(
+				literal("remove")
+				.then(
+					getItemArg()
+					.suggests(getSuggestions(name))
+					.executes(getListRemoveCommand(name))
+				)
+			)
+			.then(
+				literal("clear")
+				.executes(getListClearCommand(name))
+			);
+	}
+
+	public static void register(CommandDispatcher<CommandSource> dispatcher) {
+		dispatcher.register(
+			literal("build")
+				.then(getEnableLiteral(true))
+				.then(getEnableLiteral(false))
+				.then(getListLiteral("whitelist"))
+				.then(getListLiteral("blacklist"))
+		);
+	}
+	// @formatter:on
 	
 	private static int enable(CommandContext<CommandSource> ctx, @Nullable Collection<GameProfile> profiles, boolean state) {
 		CommandSource src = ctx.getSource();
 		MinecraftServer server = src.getServer();
-		
-		if (profiles != null) {
-			List<ServerPlayerEntity> players = profiles.stream()
-				   .map(g -> server.getPlayerList().getPlayerByUUID(g.getId()))
-				   .filter(p -> GBPlayerStore.isEnabled(p) != state)
-				   .collect(Collectors.toList());
-			players.forEach(p -> GBServerState.setEnabledFor(p, state));
-			src.sendFeedback(new StringTextComponent((state ? "Enabled" : "Disabled") + " " + GamemodeBuild.NAME + " for " + players.size() + " player(s)"), false);
-			if (state && !GBServerState.isGloballyEnabled()) {
-				src.sendFeedback(new StringTextComponent("Warning: This will have no effect as " + GamemodeBuild.NAME + " is currently globally disabled!").applyTextStyle(TextFormatting.YELLOW), false);
-			}
-			return players.size();
-		} else {
+
+		if (profiles == null) {
 			if (state == GBServerState.isGloballyEnabled()) {
 				throw new CommandException(new StringTextComponent(GamemodeBuild.NAME + " is already " + (state ? "enabled" : "disabled")));
 			}
+
 			GBServerState.setGloballyEnabled(server, state);
 			src.sendFeedback(new StringTextComponent((state ? "Enabled" : "Disabled") + " " + GamemodeBuild.NAME + " globally"), false);
 			return Command.SINGLE_SUCCESS;
 		}
-	}
-	
-	private static int addWhitelist(CommandContext<CommandSource> ctx) {
-		ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
 		
-		String entry = filter.asString();
-		GBConfigs.SERVER.addWhitelist(entry, true);
-		GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(true, true, entry));
-		ctx.getSource().sendFeedback(new StringTextComponent("Added '" + entry + "' to whitelist"), false);
-		
-		return Command.SINGLE_SUCCESS;
-	}
-	
-	private static int addBlacklist(CommandContext<CommandSource> ctx) {
-		ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
-		
-		String entry = filter.asString();
-		GBConfigs.SERVER.addBlacklist(entry, true);
-		GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(false, true, entry));
-		ctx.getSource().sendFeedback(new StringTextComponent("Added '" + entry + "' to blacklist"), false);
-		
-		return Command.SINGLE_SUCCESS;
-	}
-	
-	private static int removeWhitelist(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
-		ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
-		
-		String entry = filter.asString();
-		if (GBConfigs.SERVER.removeWhitelist(entry, true)) {
-			GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(true, false, entry));
-			ctx.getSource().sendFeedback(new StringTextComponent("Removed '" + entry + "' from whitelist"), false);
-		} else {
-			throw FILTER_DID_NOT_EXIST.create();
+		List<ServerPlayerEntity> players = profiles.stream()
+			   .map(g -> server.getPlayerList().getPlayerByUUID(g.getId()))
+			   .filter(p -> GBPlayerStore.isEnabled(p) != state)
+			   .collect(Collectors.toList());
+
+		players.forEach(p -> GBServerState.setEnabledFor(p, state));
+
+		src.sendFeedback(new StringTextComponent((state ? "Enabled" : "Disabled") + " " + GamemodeBuild.NAME + " for " + players.size() + " player(s)"), false);
+		if (state && !GBServerState.isGloballyEnabled()) {
+			src.sendFeedback(new StringTextComponent("Warning: This will have no effect as " + GamemodeBuild.NAME + " is currently globally disabled!").applyTextStyle(TextFormatting.YELLOW), false);
 		}
-		
-		return Command.SINGLE_SUCCESS;
+
+		return players.size();
 	}
-	
-	private static int removeBlacklist(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
-		ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
-		
-		String entry = filter.asString();
-		if (GBConfigs.SERVER.removeBlacklist(entry, true)) {
-			GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(false, false, entry));
-			ctx.getSource().sendFeedback(new StringTextComponent("Removed '" + entry + "' from blacklist"), false);
-		} else {
-			throw FILTER_DID_NOT_EXIST.create();
-		}
-		
-		return Command.SINGLE_SUCCESS;
-	}
-	
-	private static int clearWhitelist(CommandContext<CommandSource> ctx) {
-		int count = GBConfigs.SERVER.clearWhitelist(true);
-		GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(true, false, null));
-		
-		ctx.getSource().sendFeedback(new StringTextComponent("Removed " + count + " whitelist entries"), false);
-		
-		return Command.SINGLE_SUCCESS;
-	}
-	
-	private static int clearBlacklist(CommandContext<CommandSource> ctx) {
-		int count = GBConfigs.SERVER.clearBlacklist(true);
-		GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(false, false, null));
-		
-		ctx.getSource().sendFeedback(new StringTextComponent("Removed " + count + " blacklist entries"), false);
-		
-		return Command.SINGLE_SUCCESS;
-	}
-	
-	private static SuggestionProvider<CommandSource> whitelistSuggestions() {
-		return (ctx, builder) -> {
-			return ISuggestionProvider.suggest(GBConfigs.SERVER.whitelist(), builder);
+
+	private static Command<CommandSource> getListAddCommand(String name) {
+		return (CommandContext<CommandSource> ctx) -> {
+			ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
+			String entry = filter.asString();
+
+			GBConfigs.SERVER.addToList(name, entry, true);
+			GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(ListUpdateMessage.Operation.ADD, name, entry));
+			ctx.getSource().sendFeedback(new StringTextComponent("Added '" + entry + "' to " + name), false);
+
+			return Command.SINGLE_SUCCESS;
 		};
 	}
-	
-	private static SuggestionProvider<CommandSource> blacklistSuggestions() {
-		return (ctx, builder) -> {
-			return ISuggestionProvider.suggest(GBConfigs.SERVER.blacklist(), builder);
+
+	private static Command<CommandSource> getListRemoveCommand(String name) {
+		return (CommandContext<CommandSource> ctx) -> {
+			ItemFilterArgument.Result filter = ItemFilterArgument.getItemFilter(ctx, "item");
+			String entry = filter.asString();
+
+			if (!GBConfigs.SERVER.removeFromList(name, entry, true)) throw FILTER_DID_NOT_EXIST.create();
+
+			GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(ListUpdateMessage.Operation.REMOVE, name, entry));
+			ctx.getSource().sendFeedback(new StringTextComponent("Removed '" + entry + "' from " + name), false);
+
+			return Command.SINGLE_SUCCESS;
 		};
+	}
+
+	private static Command<CommandSource> getListClearCommand(String name) {
+		return (CommandContext<CommandSource> ctx) -> {
+			int count = GBConfigs.SERVER.clearList(name, true);
+
+			GamemodeBuild.NETWORK.send(PacketDistributor.ALL.noArg(), new ListUpdateMessage(ListUpdateMessage.Operation.CLEAR, name, null));
+			ctx.getSource().sendFeedback(new StringTextComponent("Removed " + count + " " + name + " entries"), false);
+
+			return Command.SINGLE_SUCCESS;
+		};
+	}
+
+	private static SuggestionProvider<CommandSource> getSuggestions(String name) {
+		return (ctx, builder) -> ISuggestionProvider.suggest(GBConfigs.SERVER.getListStream(name), builder);
 	}
 }
