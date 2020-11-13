f0bb9e8fefa743451afefaf2472205d7f0a3b641
tterrag <tterrag1098@gmail.com>
2020-11-13 00:20:08 -0500

    Add more lenient displaytest

--- src/main/java/com/lovetropics/gamemodebuild/GamemodeBuild.java
+++ src/main/java/com/lovetropics/gamemodebuild/GamemodeBuild.java
@@ -1,5 +1,8 @@
 package com.lovetropics.gamemodebuild;
 
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,6 +19,8 @@ import net.minecraft.command.arguments.ArgumentTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
@@ -33,17 +38,18 @@ public class GamemodeBuild {
 	public static final String NAME = "Build Mode";
 	
 	private static final Logger LOGGER = LogManager.getLogger();
-	
-	private static final String NET_PROTOCOL = "2";
-	
+
 	public static final SimpleChannel NETWORK = NetworkRegistry.ChannelBuilder.named(new ResourceLocation(MODID, "net"))
-			.networkProtocolVersion(() -> NET_PROTOCOL)
-			.clientAcceptedVersions(NET_PROTOCOL::equals)
-			.serverAcceptedVersions(NET_PROTOCOL::equals)
+			.networkProtocolVersion(GamemodeBuild::getCompatVersion)
+			.clientAcceptedVersions(GamemodeBuild::isCompatibleVersion)
+			.serverAcceptedVersions(GamemodeBuild::isCompatibleVersion)
 			.simpleChannel();
 	
 	public GamemodeBuild() {
-		// Register the setup method for modloading
+    	// Compatible with all versions that match the semver (excluding the qualifier e.g. "-beta+42")
+    	ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(GamemodeBuild::getCompatVersion, (s, v) -> GamemodeBuild.isCompatibleVersion(s)));
+
+        // Register the setup method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		// Register the doClientStuff method for modloading
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -53,7 +59,18 @@ public class GamemodeBuild {
 		
 		ModLoadingContext.get().registerConfig(Type.SERVER, GBConfigs.serverSpec);
 	}
-	
+
+    private static final Pattern QUALIFIER = Pattern.compile("-\\w+\\+\\d+");
+    public static String getCompatVersion() {
+    	return getCompatVersion(ModList.get().getModContainerById(MODID).orElseThrow(IllegalStateException::new).getModInfo().getVersion().toString());
+    }
+    private static String getCompatVersion(String fullVersion) {
+    	return QUALIFIER.matcher(fullVersion).replaceAll("");
+    }
+    public static boolean isCompatibleVersion(String version) {
+    	return getCompatVersion().equals(getCompatVersion(version));
+    }
+
 	private void setup(final FMLCommonSetupEvent event) {
 		NETWORK.messageBuilder(OpenBuildInventoryMessage.class, 0, NetworkDirection.PLAY_TO_SERVER)
 				.encoder(OpenBuildInventoryMessage::serialize).decoder(OpenBuildInventoryMessage::deserialize)
